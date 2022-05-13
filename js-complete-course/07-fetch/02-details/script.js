2321f8253c786c423e6d7e8ffba4e7770f150a0b
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-13 14:37:30 +0200

    exercise 2 of chapter 7 started but still figuring a lot out

---
+++ js-complete-course/07-fetch/02-details/script.js
@@ -0,0 +1,45 @@
+/* becode/javascript
+ *
+ * /09-fetch/03-details/script.js - 11.3: details
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+})();
+
+
+
+
+
+document.getElementById("run").addEventListener("click", () => {
+    fetch("../../_shared/api.json")
+        .then(res => res.json())
+        .then(data => {
+
+
+data.heroes
++document.getElementById("hero-id").value
+
+console.log(data);
+
+})})
+
+
+/// reading up on array.prototype.find() for figuring the rest out after some pointers from Jelle
+
+
+  
+
+
+
+
+    
+
+
+
