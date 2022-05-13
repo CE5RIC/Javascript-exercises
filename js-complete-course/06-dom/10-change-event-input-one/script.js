9bbba0c3ed72c542fabc8e52c3f1aafae96f47c4
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-13 09:07:41 +0200

    solved exercise 10 of chapter 6

---
+++ js-complete-course/06-dom/10-change-event-input-one/script.js
@@ -0,0 +1,30 @@
+/* becode/javascript
+ *
+ * /06-dom/11-change-event-input-one/script.js - 6.11: événement change (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+
+
+(function() {
+
+    let password = document.getElementById('pass-one');
+    let counter = document.getElementById('counter');
+    password.maxLength="10"
+
+    password.addEventListener('input', () => {
+        let passLength = password.value.length;
+        counter.innerHTML = passLength + '/10';
+    })
+
+})();
+
+
+
+
+
