3d272c3e020f4180c417b51521de06f0bb448102
CE5RIC <cedricdierckx5@hotmail.com>
2022-05-21 11:41:08 +0200

    solved optional exercise 4 of chapter 4

---
+++ js-complete-course/04-maths/04-even-square-Optional/script.js
@@ -0,0 +1,28 @@
+/* becode/javascript
+ *
+ * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+for (let i = 0; i < numbersArray.length; i++) {
+    console.log(i * i);
+}
+
+    });
+
+})();
+
+
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+
